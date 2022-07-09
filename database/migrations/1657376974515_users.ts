0f6cf2b0c69c25727186140edd6f481a677bc3fb
jgbpessoa <jgbpessoa@gmail.com>
2022-07-09 11:42:57 -0300

    Added name field on User (model & migration)

--- database/migrations/1657376974515_users.ts
+++ database/migrations/1657376974515_users.ts
@@ -6,9 +6,9 @@ export default class UsersSchema extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary()
+      table.string('name').notNullable()
       table.string('email', 255).notNullable()
       table.string('password', 180).notNullable()
-      table.string('remember_me_token').nullable()
 
       /**
        * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
