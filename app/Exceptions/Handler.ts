ec22ff80cf58e5573cef3f146220e51e60833d59
jgbpessoa <jgbpessoa@gmail.com>
2022-07-10 02:25:17 -0300

    Set Adonisjs Error Handling (Handler.ts)

--- app/Exceptions/Handler.ts
+++ app/Exceptions/Handler.ts
@@ -15,9 +15,21 @@
 
 import Logger from '@ioc:Adonis/Core/Logger'
 import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class ExceptionHandler extends HttpExceptionHandler {
   constructor() {
     super(Logger)
+  }
+
+  public async handle(error, ctx: HttpContextContract) {
+    if (error.code === 'E_ROW_NOT_FOUND') {
+      return ctx.response.notFound({ message: 'Data not found' })
+    }
+
+    if (error.code === 'E_UNAUTHORIZED_ACCESS') {
+      return ctx.response.unauthorized({ message: 'User Authentication is required' })
+    }
+    return super.handle(error, ctx)
   }
 }
